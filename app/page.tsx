7d71540c18aa4f31d77e25d2db30e13d3562b842
JiHoon-0330 <ljihoon0330@gamil.com>
2022-10-29 21:46:09 +0900

    페이지 위치 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { rest } from "apis";
 import { use } from "react";
-import WeverseClient from "./client";
-import WeverseList from "./weverse-list";
+import WeverseClient from "./(weverse)/client";
+import WeverseList from "./(weverse)/weverse-list";
 
 const Page = () => {
   const { data, lastId } = use(rest.getWeverse());
